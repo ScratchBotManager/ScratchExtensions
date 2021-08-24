b892ae20fe44a2c61501414865b42fcdb9d97cbe
HackitHatchet <84136976+ScratchBotManager@users.noreply.github.com>
2021-08-24 19:40:29 -0400

    Update test.js

--- test.js
+++ test.js
@@ -1,1 +1,40 @@
-ext._shutdown = function() {};ext._getStatus = function() { return {status: 2, msg: 'Ready'};}; ext.test = function (callback){ alert("Hello!") }; var descriptor = { blocks: [ [" ","( hello)","test"],],menus: {},url: 'https://kyleplo.github.io/scratch-extension-creator/help#'Hello!};ScratchExtensions.register('My Extension', descriptor, ext);
+(function (ext) {
+	var ExtName = 'Test';
+	ext._shutdown = function() {};
+	ext._getStatus = function() {
+		return {status: 2, msg: 'Ready'};
+	};
+	var descriptor = {
+	        blocks: [
+	            ['r', 'Hello World', 'secId1'],
+	            ['r', 'Hello World', 'secId2'],
+	            ['r', 'Hello World', 'secId3'],
+	            ['r', 'Hello World', 'secId4'],
+	            [' ', 'Hello World', 'secId5'],
+	        ],
+			menus: {
+			},
+	        url: 'https://scratch.mit.edu'
+	};
+	ext.secId1 = function(ext,callback){
+		
+		callback();
+	};
+	ext.secId2 = function(ext,callback){
+		
+		callback();
+	};
+	ext.secId3 = function(ext,callback){
+		
+		callback();
+	};
+	ext.secId4 = function(ext,callback){
+		
+		callback();
+	};
+	ext.secId5 = function(ext,callback){
+		alert("Hello World");
+		callback();
+	};
+	ScratchExtensions.register(ExtName, descriptor, ext);
+})({});
