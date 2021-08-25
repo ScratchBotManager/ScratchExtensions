083171fae51903d9b130bde1d14274a45ec64d2c
HackitHatchet <84136976+ScratchBotManager@users.noreply.github.com>
2021-08-25 12:56:41 -0400

    Update IfReporter.js

--- IfReporter.js
+++ IfReporter.js
@@ -1,20 +1,44 @@
-(function (ext) {
-	var ExtName = 'IfReporter';
-	ext._shutdown = function() {};
-	ext._getStatus = function() {
-		return {status: 2, msg: 'Ready'};
-	};
-	var descriptor = {
-	        blocks: [
-	            ['r', 'if %b then %s else %s', 'secId1'],
-	        ],
-			menus: {
-			},
-	        url: ''
-	};
-	ext.secId1 = function(ext,callback){
-		
-		callback();
-	};
-	ScratchExtensions.register(ExtName, descriptor, ext);
-})({});
+class IfReporter {
+    //Converted from https://scratchbotmanager.github.io/ScratchExtensions/IfReporter.js to Scratch 3.0 using Ext2to3!
+    getInfo() {
+        return {
+            "id": "IfReporter",
+            "name": "IfReporter",
+            "blocks": [{
+                "opcode": "secId1",
+                "blockType": "reporter",
+                "text": "if [ext] then [callback] else [undefined]",
+                "arguments": {
+                    "ext": {
+                        "type": "Boolean"
+                    },
+                    "callback": {
+                        "type": "string"
+                    },
+                    "undefined": {
+                        "type": "string"
+                    }
+                }
+            }],
+            "menus": {}
+        };
+    }
+    secId1({
+        ext,
+        callback
+    }) {
+
+        callback();
+    }
+    _formatMenu(menu) {
+        const m = [];
+        for (let i = 0; i < menu.length; i++) {
+            const obj = {};
+            obj.text = menu[i];
+            obj.value = i.toString();
+            m.push(obj);
+        }
+        return m;
+    }
+}
+Scratch.extensions.register(new IfReporter());
