0068ddd63db40fc09d34a7d1074ca0ef99cc2180
HackitHatchet <84136976+ScratchBotManager@users.noreply.github.com>
2021-08-27 22:30:08 -0400

    Update JavaScript.js

--- JavaScript.js
+++ JavaScript.js
@@ -14,6 +14,7 @@ class Javascript {
                 "arguments": {
                     "ext": {
                         "type": "string"
+                        "defaultValue": 'alert("Hello World");'
                     }
                 }
             }, {
@@ -22,7 +23,8 @@ class Javascript {
                 "text": "Javascript Function [ext]",
                 "arguments": {
                     "ext": {
-                        "type": "string"
+                        "type": "string",
+                        "defaultValue": 'alert("Hello World");'
                     }
                 }
             }],
