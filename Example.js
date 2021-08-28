577d2cf9a8120a569dc29876e5e3793c3d937e38
HackitHatchet <84136976+ScratchBotManager@users.noreply.github.com>
2021-08-27 22:04:45 -0400

    Update Example.js

--- Example.js
+++ Example.js
@@ -1,20 +1,34 @@
-(function (ext) {
-	var ExtName = 'Test';
-	ext._shutdown = function() {};
-	ext._getStatus = function() {
-		return {status: 2, msg: 'Ready'};
-	};
-	var descriptor = {
-	        blocks: [
-	            [' ', 'Test Block', 'secId1'],
-	        ],
-			menus: {
-			},
-	        url: ''
-	};
-	ext.secId1 = function(ext,callback){
-		alert("Hi");
-		callback();
-	};
-	ScratchExtensions.register(ExtName, descriptor, ext);
-})({});
+class Test {
+    //Converted from https://scratchbotmanager.github.io/ScratchExtensions/Example.js to Scratch 3.0 using Ext2to3!
+    getInfo() {
+        return {
+            "id": "Test",
+            "name": "Test",
+            "blocks": [{
+                "opcode": "secId1",
+                "blockType": "command",
+                "text": "Test Block",
+                "arguments": {}
+            }],
+            "menus": {}
+        };
+    }
+    secId1({
+        ext,
+        callback
+    }) {
+        alert("Hi");
+        callback();
+    }
+    _formatMenu(menu) {
+        const m = [];
+        for (let i = 0; i < menu.length; i++) {
+            const obj = {};
+            obj.text = menu[i];
+            obj.value = i.toString();
+            m.push(obj);
+        }
+        return m;
+    }
+}
+Scratch.extensions.register(new Test());
