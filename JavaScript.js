5ab00315df0f9750778b8eb84620510ca53fe8ab
HackitHatchet <84136976+ScratchBotManager@users.noreply.github.com>
2021-08-28 05:16:36 -0400

    Update JavaScript.js

--- JavaScript.js
+++ JavaScript.js
@@ -50,8 +50,11 @@ class Javascript {
        return code;
     }
     secId3({var1}) {
-       var code = document.createElement("script"); s.type = "text/javascript"; s.src = var1; $("head").append(s);;
-       return code;
+        import * as $ from "https://code.jquery.com/jquery-3.6.0.min.js"
+       document.createElement("script"); 
+        s.type = "text/javascript"; 
+        s.src = var1; 
+        $("head").append(s);;
     }
 }
 Scratch.extensions.register(new Javascript());
