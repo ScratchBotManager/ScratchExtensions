56a58c3dfde6db24eba867d9c5500f415798327f
HackitHatchet <84136976+ScratchBotManager@users.noreply.github.com>
2021-08-27 21:46:23 -0400

    Update JavaScript.js

--- JavaScript.js
+++ JavaScript.js
@@ -1,25 +1,54 @@
-(function (ext) {
-	var ExtName = 'Javascript';
-	ext._shutdown = function() {};
-	ext._getStatus = function() {
-		return {status: 2, msg: 'Ready'};
-	};
-	var descriptor = {
-	        blocks: [
-	            [' ', 'Run %s', 'secId1'],
-	            ['r', 'Javascript Function %s', 'secId2'],
-	        ],
-			menus: {
-			},
-	        url: ''
-	};
-	ext.secId1 = function(ext,callback){
-		
-		callback();
-	};
-	ext.secId2 = function(ext,callback){
-		
-		callback();
-	};
-	ScratchExtensions.register(ExtName, descriptor, ext);
-})({});
+class Javascript {
+    //Converted from https://scratchbotmanager.github.io/ScratchExtensions/JavaScript.js to Scratch 3.0 using Ext2to3!
+    getInfo() {
+        return {
+            "id": "Javascript",
+            "name": "Javascript",
+            "blocks": [{
+                "opcode": "secId1",
+                "blockType": "command",
+                "text": "Run [ext]",
+                "arguments": {
+                    "ext": {
+                        "type": "string"
+                    }
+                }
+            }, {
+                "opcode": "secId2",
+                "blockType": "reporter",
+                "text": "Javascript Function [ext]",
+                "arguments": {
+                    "ext": {
+                        "type": "string"
+                    }
+                }
+            }],
+            "menus": {}
+        };
+    }
+    secId1({
+        ext,
+        callback
+    }) {
+        return ext;
+        callback();
+    }
+    secId2({
+        ext,
+        callback
+    }) {
+       return ext;
+        callback();
+    }
+    _formatMenu(menu) {
+        const m = [];
+        for (let i = 0; i < menu.length; i++) {
+            const obj = {};
+            obj.text = menu[i];
+            obj.value = i.toString();
+            m.push(obj);
+        }
+        return m;
+    }
+}
+Scratch.extensions.register(new Javascript());
