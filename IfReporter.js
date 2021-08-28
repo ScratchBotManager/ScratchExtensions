428a6ce543bdacbe4261641f767a8e042614aa1d
HackitHatchet <84136976+ScratchBotManager@users.noreply.github.com>
2021-08-27 21:12:04 -0400

    Update IfReporter.js

--- IfReporter.js
+++ IfReporter.js
@@ -23,13 +23,22 @@ class IfReporter {
             "menus": {}
         };
       }
-}
-    }
-    IfThenElse({ifbool, then, elsestr})
+        IfThenElse({ifbool, then, elsestr})
     if (ifbool == true) {
         return then;
     } else {
         return elsestr;
            }
+      _formatMenu(menu) {
+        const m = [];
+        for (let i = 0; i < menu.length; i++) {
+            const obj = {};
+            obj.text = menu[i];
+            obj.value = i.toString();
+            m.push(obj);
+        }
+        return m;
+    }
+
 }
 Scratch.extensions.register(new IfReporter());
