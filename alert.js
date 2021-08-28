0b3082b8a17f9473a25909686efe0be4c4f4ae01
HackitHatchet <84136976+ScratchBotManager@users.noreply.github.com>
2021-08-28 18:25:41 -0400

    Update alert.js

--- alert.js
+++ alert.js
@@ -1,25 +1,55 @@
-(function (ext) {
-	var ExtName = 'Alert';
-	ext._shutdown = function() {};
-	ext._getStatus = function() {
-		return {status: 2, msg: 'Ready'};
-	};
-	var descriptor = {
-	        blocks: [
-	            [' ', 'Alert %s', 'secId1'],
-	            ['r', 'Prompt %s', 'secId2'],
-	        ],
-			menus: {
-			},
-	        url: ''
-	};
-	ext.secId1 = function(ext,callback){
-		
-		callback();
-	};
-	ext.secId2 = function(ext,callback){
-		
-		callback();
-	};
-	ScratchExtensions.register(ExtName, descriptor, ext);
-})({});
+class Alert {
+    //Converted from https://scratchbotmanager.github.io/ScratchExtensions/alert.js to Scratch 3.0 using Ext2to3!
+    getInfo() {
+        return {
+            "id": "Alert",
+            "name": "Alert",
+            "blocks": [{
+                "opcode": "secId1",
+                "blockType": "command",
+                "text": "Alert [ext]",
+                "arguments": {
+                    "ext": {
+                        "type": "string"
+                    }
+                }
+            }, {
+                "opcode": "secId2",
+                "blockType": "reporter",
+                "text": "Prompt [ext]",
+                "arguments": {
+                    "ext": {
+                        "type": "string"
+                    }
+                }
+            }],
+            "menus": {}
+        };
+    }
+    secId1({
+        ext,
+        callback
+    }) {
+        alert(ext);
+        callback();
+    }
+    secId2({
+        ext,
+        callback
+    }) {
+        var x = prompt(ext);
+	return x
+        callback();
+    }
+    _formatMenu(menu) {
+        const m = [];
+        for (let i = 0; i < menu.length; i++) {
+            const obj = {};
+            obj.text = menu[i];
+            obj.value = i.toString();
+            m.push(obj);
+        }
+        return m;
+    }
+}
+Scratch.extensions.register(new Alert());
