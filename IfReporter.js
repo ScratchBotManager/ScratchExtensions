745c628c6e9f4ef984e490cea09d6eb95704d3ad
HackitHatchet <84136976+ScratchBotManager@users.noreply.github.com>
2021-08-28 00:53:51 -0400

    Update IfReporter.js

--- IfReporter.js
+++ IfReporter.js
@@ -1,20 +1,37 @@
-(function (ext) {
-	var ExtName = 'IfReporter';
-	ext._shutdown = function() {};
-	ext._getStatus = function() {
-		return {status: 2, msg: 'Ready'};
-	};
-	var descriptor = {
-	        blocks: [
-	            ['r', 'if %b then %s else %s', 'secId1'],
-	        ],
-			menus: {
-			},
-	        url: ''
-	};
-	ext.secId1 = function(ifbool,then,elsestr){
-		if (ifbool == true) {return then} else {return elsestr}
-		callback();
-	};
-	ScratchExtensions.register(ExtName, descriptor, ext);
-})({});
+class IfReporter {
+    //Converted from https://scratchbotmanager.github.io/ScratchExtensions/IfReporter.js to Scratch 3.0 using Ext2to3!
+    getInfo() {
+        return {
+            "id": "IfReporter",
+            "name": "IfReporter",
+            "blocks": [{
+                "opcode": "secId1",
+                "blockType": "reporter",
+                "text": "if [ifbool] then [then] else [elsestr]",
+                "arguments": {
+                    "ifbool": {
+                        "type": "Boolean"
+                    },
+                    "then": {
+                        "type": "string"
+                    },
+                    "elsestr": {
+                        "type": "string"
+                    }
+                }
+            }],
+            "menus": {}
+        };
+    }
+    secId1({
+        ifbool,
+        then,
+        elsestr
+    }) {
+        if (ifbool == true) {
+            return then
+        } else {
+            return elsestr
+        }
+}
+Scratch.extensions.register(new IfReporter());
