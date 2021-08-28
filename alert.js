c0bdbbd5c34988f59ff67292c947a1d61f74c186
HackitHatchet <84136976+ScratchBotManager@users.noreply.github.com>
2021-08-28 18:21:00 -0400

    Update alert.js

--- alert.js
+++ alert.js
@@ -1,1 +1,25 @@
-alert("Hello World");
+(function (ext) {
+	var ExtName = 'Alert';
+	ext._shutdown = function() {};
+	ext._getStatus = function() {
+		return {status: 2, msg: 'Ready'};
+	};
+	var descriptor = {
+	        blocks: [
+	            [' ', 'Alert %s', 'secId1'],
+	            ['r', 'Prompt %s', 'secId2'],
+	        ],
+			menus: {
+			},
+	        url: ''
+	};
+	ext.secId1 = function(ext,callback){
+		
+		callback();
+	};
+	ext.secId2 = function(ext,callback){
+		
+		callback();
+	};
+	ScratchExtensions.register(ExtName, descriptor, ext);
+})({});
